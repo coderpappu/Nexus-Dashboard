d640d867ec50b11af862e80cc6dd46db3c85ba53
coderpappu <pappudey.coder@gmail.com>
2024-04-01 00:00:14 +0600

    vercel web analytics added

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { ContextProvider } from "./contexts/ContextProvider";
-
+import { Analytics } from "@vercel/analytics/react";
 ReactDOM.render(
   <ContextProvider>
     <App />
+    <Analytics />
   </ContextProvider>,
   document.getElementById("root")
 );
