dfad35fd10ca09a2114cfa6f0e69eeff80b00831
coderpappu <pappudey.coder@gmail.com>
2024-06-05 23:26:47 +0600

    here now i added a full overview of week . responsive design complete now just data change task is pending

--- dashboard/src/pages/dashboard/Analytics.jsx
+++ dashboard/src/pages/dashboard/Analytics.jsx
@@ -6,6 +6,11 @@ import SalesOverview from "../../components/analytics/webAnalytics/SalesOverview
 import Revenue from "../../components/analytics/webAnalytics/Revenue.jsx";
 import { Card } from "keep-react";
 import { BarChart } from "keep-react";
+import { FiDollarSign, FiPieChart } from "react-icons/fi";
+import { RiPaypalLine } from "react-icons/ri";
+
+import { LineProgress } from "keep-react";
+
 const BarChartData = [
   {
     name: "2",
@@ -39,33 +44,8 @@ const BarChartData = [
     sell: 160,
   },
   {
-    name: "20",
-    price: 390,
-    sell: 110,
-  },
-  {
-    name: "22",
-    price: 220,
-    sell: 90,
-  },
-  {
-    name: "24",
-    price: 360,
-    sell: 170,
-  },
-  {
-    name: "26",
-    price: 410,
-    sell: 140,
-  },
-  {
-    name: "28",
+    name: "18",
     price: 280,
-    sell: 200,
-  },
-  {
-    name: "30",
-    price: 330,
     sell: 160,
   },
 ];
@@ -73,7 +53,11 @@ const Analytics = () => {
   const { activeMenu } = useStateContext();
 
   return (
-    <div className="m-2 md:m-0 mt-24 p-2 md:p-10 font-public">
+    <div
+      className={`m-2 md:m-0 mt-24 p-2 md:p-10 font-public ${
+        !activeMenu && "xl:w-[1530px] xl:m-auto"
+      }`}
+    >
       {/* First row */}
       <div className="flex flex-wrap justify-around w-full overflow-hidden relative">
         {/* Website analytics */}
@@ -93,20 +77,30 @@ const Analytics = () => {
       </div>
 
       {/* 2nd row  */}
-      <div className="flex flex-wrap ">
+      <div className="flex flex-wrap my-5">
         <div className="w-[600px] h-auto bg-white p-4 rounded-md">
-          <div>
-            <h2 className="text-lg font-medium ">Earning Reports</h2>
-            <p className="text-sm font-normal">Weekly Earning Overview</p>
+          <div className="mb-6 md:mb-0">
+            <h2 className="text-lg font-medium text-[#5D596C]">
+              Earning Reports
+            </h2>
+            <p className="text-sm font-normal text-[#A5A3AE]">
+              Weekly Earning Overview
+            </p>
           </div>
           <div className="flex flex-wrap w-full justify-between ">
-            <div className="w-[40%] h-auto mt-auto">
-              <h2 className="font-medium text-4xl">$256</h2>
-              <p className="text-sm ">
+            <div className="w-full md:w-[40%] h-auto mb-5 md:mb-0 mt-auto ">
+              <div className="flex flex-wrap items-center mb-4">
+                <h2 className="font-medium text-4xl  text-[#5D596C]">$256</h2>
+
+                <div className="w-16 h-6 bg-[#DFF7E9] text-[#43CE81] rounded-md text-center text-[13px] p-1 ml-2 ">
+                  +4.2%
+                </div>
+              </div>
+              <p className="text-sm text-[#777485]">
                 You informed of this week compared to last week
               </p>
             </div>
-            <div className="w-[60%] h-auto">
+            <div className="w-full md:w-[60%] ">
               <BarChart
                 height={200}
                 width={350}
@@ -115,18 +109,62 @@ const Analytics = () => {
                 barRadius={[4, 4, 0, 0]}
                 active={true}
                 activeIndex={5}
-                activeColor="#1B4DFF"
-                inActiveColor="#94ABFF"
+                activeColor="#6D64BC"
+                inActiveColor="#EFEFFF"
               />
             </div>
           </div>
-          <div className="border-1 border-cyan-100 rounded-md">
-            <div>
-              <div>
+          <div className="border-1 border-[#DBDADE] rounded-md flex flex-wrap mt-6">
+            <div className="w-full md:w-[33%] p-4">
+              <div className="flex flex-wrap items-center mb-2">
                 {/* icon  */}
-                <h2>Earnings</h2>
+                <div className="w-6 h-6  bg-[#EAE8FD] rounded-sm ">
+                  <FiDollarSign className="text-md m-auto mt-1 text-[#786DF1]" />
+                </div>
+                <h2 className="ml-2 text-[#5F5B6E]">Earnings</h2>
               </div>
-              <h2>$3422</h2>
+              <h2 className="text-2xl mb-2 text-[#5D596C]">$342.67</h2>
+              <LineProgress
+                progress={62.2}
+                strokeBgColor="red"
+                size="sm"
+                className=" bg-[#7367F0]"
+              ></LineProgress>
+              {/* here is a line  */}
+            </div>
+
+            <div className="w-full md:w-[33%] p-4">
+              <div className="flex flex-wrap items-center mb-2">
+                {/* icon  */}
+                <div className="w-6 h-6  bg-[#EAE8FD] rounded-sm ">
+                  <FiDollarSign className="text-md m-auto mt-1 text-[#786DF1]" />
+                </div>
+                <h2 className="ml-2">Earnings</h2>
+              </div>
+              <h2 className="text-2xl mb-2">$342.67</h2>
+              <LineProgress
+                progress={62.2}
+                strokeBgColor="red"
+                size="sm"
+                className=" bg-[#7367F0]"
+              ></LineProgress>
+              {/* here is a line  */}
+            </div>
+            <div className="w-full md:w-[33%] p-4">
+              <div className="flex flex-wrap items-center mb-2">
+                {/* icon  */}
+                <div className="w-6 h-6  bg-[#EAE8FD] rounded-sm ">
+                  <FiDollarSign className="text-md m-auto mt-1 text-[#786DF1]" />
+                </div>
+                <h2 className="ml-2">Earnings</h2>
+              </div>
+              <h2 className="text-2xl mb-2">$342.67</h2>
+              <LineProgress
+                progress={62.2}
+                strokeBgColor="red"
+                size="sm"
+                className=" bg-[#7367F0]"
+              ></LineProgress>
               {/* here is a line  */}
             </div>
           </div>
